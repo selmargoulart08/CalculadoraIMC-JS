c18753578dc665e82369f8f096ebce43d0efb3c4
devSelmaJr <60160024+selmargoulart08@users.noreply.github.com>
2023-05-17 23:04:29 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ form.addEventListener('submit', function (e) {
 });
 
 function getNivelImc (imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+  const nivel = ['Abaixo do peso(bem sequinho)', 'Peso normal', 'Sobrepeso',
     'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3,Cuidado!'];
 
   if (imc >= 39.9) return nivel[5];
